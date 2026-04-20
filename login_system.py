281aa2db6fd25003fdd62c40761fb284df8cad2c
sabrinaahmed2707-dot <sabrina.ahmed2707@gmail.com>
2026-04-20 15:41:18 +0400

    Update login_system.py
    updated login logic with salted password verification

--- login_system.py
+++ login_system.py
@@ -3,15 +3,19 @@ import hashlib
 users = {}
 attempts = 3
 
+import os
+
 def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
+    salt = os.urandom(16)
+    hashed = hashlib.sha256(salt + password.encode()).hexdigest()
+    return salt, hashed
 
 def register():
     username = input("Enter username: ")
     password = input("Enter password: ")
     
-    hashed = hash_password(password)
-    users[username] = hashed
+   salt, hashed = hash_password(password)
+   users[username] = (salt, hashed)
     
     print("User registered successfully!\n")
 
@@ -21,18 +25,20 @@ def login():
     username = input("Enter username: ")
     password = input("Enter password: ")
     
-    hashed = hash_password(password)
-    
-    if username in users and users[username] == hashed:
-        print("Login successful!\n")
-        attempts = 3
-    else:
-        attempts -= 1
-        print(f"Invalid credentials! Attempts left: {attempts}\n")
+    if username in users:
+        salt, stored_hash = users[username]
+        new_hash = hashlib.sha256(salt + password.encode()).hexdigest()
         
-        if attempts == 0:
-            print("Too many failed attempts. Access temporarily blocked.\n")
-
+        if new_hash == stored_hash:
+            print("Login successful!\n")
+            attempts = 3
+            return
+    
+    attempts -= 1
+    print(f"Invalid credentials! Attempts left: {attempts}\n")
+    
+    if attempts == 0:
+        print("Too many failed attempts. Access temporarily blocked.\n")
 def main():
     while True:
         print("1. Register")
